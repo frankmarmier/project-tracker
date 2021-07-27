108ae3d42175589e2e8f6baf0c48fc7ceabb8679
1testsample <frankomarmier@gmail.com>
2021-07-27 18:05:39 +0200

    p

--- routes/projects.js
+++ routes/projects.js
@@ -1,114 +1,116 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Technology = require('../models/Technology')
-const User = require('../models/User')
-const Project = require('../models/Project.js')
+const Technology = require("../models/Technology");
+const User = require("../models/User");
+const Project = require("../models/Project.js");
 
 //GET
 
-router.get('/', async (req, res, next) => {
-    try{
-        const data = await Project.find()
-        console.log('DATA', data)
-        res.render('projects/list-projects', {projects: data})
-    }
-    catch(err){
-        console.log('Failed to load projects')
-        next(err)
-    }
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await Project.find();
+    console.log("DATA", data);
+    res.render("projects/list-projects", { projects: data });
+  } catch (err) {
+    console.log("Failed to load projects");
+    next(err);
+  }
+});
 
-router.get('/project/create', (req, res, next) => {
-    
-    User.find()
-        .then(userDocs => {
-        Technology.find()
-            .then(techDocs => {
-            res.render('projects/project-create', {
-                technology: techDocs,
-                contributor: userDocs
-                })
-            })
-            .catch(e => console.log(e))
+router.get("/create", (req, res, next) => {
+  User.find()
+    .then((userDocs) => {
+      Technology.find()
+        .then((techDocs) => {
+          res.render("projects/project-create", {
+            technology: techDocs,
+            contributor: userDocs,
+          });
         })
-        .catch(e => console.log(e))
-    
-    
-    
-})
+        .catch((e) => console.log(e));
+    })
+    .catch((e) => console.log(e));
+});
 
-router.get('/project/:id', async (req, res, next) => {
-    try{
-        const projectId = req.params.id
-        const myProject = await Project.findById(projectId)
-        res.render('projects/one-project', {myProject})
-    }
-    catch(err){
-        console.log("Couldn't get the project")
-        next(err)
-    }  
-})
+router.get("/:id", async (req, res, next) => {
+  try {
+    const projectId = req.params.id;
+    const myProject = await Project.findById(projectId).populate(
+      "contributors"
+    );
+    console.log(myProject);
+    res.render("projects/one-project", { myProject });
+  } catch (err) {
+    console.log("Couldn't get the project");
+    next(err);
+  }
+});
 
-router.get('/project/:id/update', async (req, res, next) => {
-    try{
-        const projectId = req.params.id
-        const myProject = await Project.findById(projectId)
-        const technologies = await Technology.find()
-        const isIncluded = []
-        technologies.forEach(tech => {
-            if (myProject.technologies.includes(tech)) {
-                isIncluded.push(true)
-            } else {
-                isIncluded.push(false)
-            }
-        })
+router.get("/:id/update", async (req, res, next) => {
+  try {
+    const projectId = req.params.id;
+    const users = await User.find();
+    const myProject = await Project.findById(projectId).populate(
+      "contributors"
+    );
 
-        res.render('projects/update-project', {myProject, technologies, isIncluded})
-    }
-    catch(err){
-        console.log("Couldn't get the project")
-        next(err)
-    }  
-})
+    const technologies = await Technology.find();
+    const isIncluded = [];
 
-router.get('/project/:id/delete', async (req, res, next) => {
-    try{
-        const projectId = req.params.id
-         await Project.findByIdAndDelete(projectId)
-        res.redirect('/projects')
-    }
-    catch(err){
-        console.log("Couldn't get the project")
-        next(err)
-    }  
-})
+    technologies.forEach((tech) => {
+      if (myProject.technology.includes(tech)) {
+        isIncluded.push(true);
+      } else {
+        isIncluded.push(false);
+      }
+    });
+
+    res.render("projects/update-project", {
+      myProject,
+      technologies,
+      isIncluded,
+      users,
+    });
+  } catch (err) {
+    console.log("Couldn't get the project");
+    next(err);
+  }
+});
+
+router.get("/:id/delete", async (req, res, next) => {
+  try {
+    const projectId = req.params.id;
+    await Project.findByIdAndDelete(projectId);
+    res.redirect("/projects");
+  } catch (err) {
+    console.log("Couldn't get the project");
+    next(err);
+  }
+});
 
 //POST
 
-router.post('/project/create', async (req, res, next) => {
-    try{
-        const data = req.body
-        await Project.create(data)
-    }
-    catch(err){
-        console.log('Err creating new project')
-        next(err)
-    }
-})
+router.post("/create", async (req, res, next) => {
+  try {
+    const data = req.body;
+    await Project.create(data);
+    res.redirect("/projects");
+  } catch (err) {
+    console.log("Err creating new project");
+    next(err);
+  }
+});
 
-router.post('/project/:id/update', async (req, res, next) => {
-    try{
-        const projectId = req.params.id
-        const data = req.body
-        const myProject = await Project.findByIdAndUpdate(projectId, data)
-        res.redirect('/project/:id')
-    }
-    catch(err){
-        console.log("Err updating project")
-        next(err)
-    }  
-})
+router.post("/:id/update", async (req, res, next) => {
+  try {
+    const projectId = req.params.id;
+    const data = req.body;
+    const myProject = await Project.findByIdAndUpdate(projectId, data);
+    res.redirect("/project/:id");
+  } catch (err) {
+    console.log("Err updating project");
+    next(err);
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
--- routes/technology.js
+++ routes/technology.js
@@ -1,21 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const technology = require("../models/Technology.js");
 
-
-router.get ('/', (req, res, next) =>{
-    technology.find().then(technologyfound => {res.render("technology.hbs", {technology: technologyfound,})}).catch((error) => {
-        console.log(error)
+router.get("/", (req, res, next) => {
+  technology
+    .find()
+    .then((technologyfound) => {
+      res.render("technology.hbs", { technology: technologyfound });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 });
 
-router.post('/:id/delete', (req, res, next) => {
-    technology.findByIdAndDelete(req.params.id).then(() => {
-        res.redirect()
-    }).catch((error) => {
-console.log(error)
+router.post("/:id/delete", (req, res, next) => {
+  technology
+    .findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/technology");
+    })
+    .catch((error) => {
+      console.log(error);
     });
 });
-
 
 module.exports = router;
