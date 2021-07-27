afd1c026f93656900aa52eb981e6665b1f313d2a
Mickaelle Pazuelo <mickaelle.pazuelo@gmail.com>
2021-07-27 17:21:37 +0200

    technology

--- routes/technology.js
+++ routes/technology.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const technology = require("../models/technology.js");
 
 
-
 router.get ('/', (req, res, next) =>{
     technology.find().then(technologyfound => {res.render("technology.hbs", {technology: technologyfound,})}).catch((error) => {
         console.log(error)
     });
 });
 
+router.post('/:id/delete', (req, res, next) => {
+    technology.findByIdAndDelete(req.params.id).then(() => {
+        res.redirect()
+    }).catch((error) => {
+console.log(error)
+    });
+});
 
 
 module.exports = router;
